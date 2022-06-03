05832521f8d3ba3d5f4af1b838508b71c8197fea
Hassan Alkhalidi <gruceing@gmail.com>
2022-06-03 22:02:09 +0300

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const {
+    app,
+    BrowserWindow
+} = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        titleBarStyle: 'hidden',
+        titleBarOverlay: {
+            color: '#f8fbff',
+            symbolColor: '#333333ff'
+        }
+    })
+
+    win.loadURL('https://site.gruceing.dev')
+}
+
+app.whenReady().then(() => {
+    createWindow()
+})
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
